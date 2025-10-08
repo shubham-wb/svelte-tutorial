55956ad8b2acbe5003608626bbda9a90cc9350cc
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-08 06:41:00 +0530

    feature/template

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let count: number = $state(0);
+  const increment = () => {
+    count += 1;
+  };
+
+  let title = "Hello Shubham";
+</script>
+
+<button onclick={increment}>
+  count is {count}
+</button>
+<h1>
+  {title}
+</h1>
