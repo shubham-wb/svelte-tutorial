f95c34f7f3d5a1b3c3b46e08265c92e508648563
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-23 18:58:36 +0530

    feature/derived-state

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script>
-  let count = $state(0);
+  let numbers = [1, 2, 3, 4];
 
-  function increment() {
-    // TODO implement
-    count++;
+  function addNumber() {
+    numbers[numbers.length] = numbers.length + 1;
   }
+  //   $: sum = numbers.reduce((a, b) => a + b, 0);
+  let total = $derived(numbers.reduce((a, b) => a + b, 0));
+
+  //   `$:` is not allowed in runes mode, use `$derived` or `$effect` instead
+  //   let sum = numbers.reduce((a, b) => a + b, 0);
+  //this is not reactive we need to menition either derived or $: before the variable
 </script>
 
-<button onclick={increment}>
-  Clicked {count}
-  {count === 1 ? "time" : "times"}
-</button>
+<p>{numbers.join(" + ")} = {total}</p>
+
+<button onclick={addNumber}> Add a number </button>
