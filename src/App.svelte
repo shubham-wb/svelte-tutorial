7845744923756a10b0f5608367b5e96fc4249ef6
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-23 19:07:04 +0530

    feature/inspect

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,10 @@
   function addNumber() {
     numbers[numbers.length] = numbers.length + 1;
   }
-  //   $: sum = numbers.reduce((a, b) => a + b, 0);
+
   let total = $derived(numbers.reduce((a, b) => a + b, 0));
 
-  //   `$:` is not allowed in runes mode, use `$derived` or `$effect` instead
-  //   let sum = numbers.reduce((a, b) => a + b, 0);
-  //this is not reactive we need to menition either derived or $: before the variable
+  $inspect(numbers, "Numbers array").with(console.trace);
 </script>
 
 <p>{numbers.join(" + ")} = {total}</p>
