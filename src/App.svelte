d8cb104aafa31ca65c31e3adad2ecc490c06889c
tensaidevx <shubham.gupta.npd@gmail.com>
2025-10-25 22:13:40 +0530

    keys in mapping

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,17 @@
 <script>
-  const colors = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "indigo",
-    "violet",
-  ];
-  let selected = $state(colors[0]);
+  import Thing from "./Thing.svelte";
+
+  let things = $state([
+    { id: 1, name: "apple" },
+    { id: 2, name: "banana" },
+    { id: 3, name: "carrot" },
+    { id: 4, name: "doughnut" },
+    { id: 5, name: "egg" },
+  ]);
 </script>
 
-<h1 style="color: {selected}">Pick a colour</h1>
+<button onclick={() => things.shift()}> Remove first thing </button>
 
-<div>
-  {#each colors as color, index}
-    <button
-      style="background: {color}"
-      aria-label={color}
-      aria-current={selected === color}
-      onclick={() => (selected = color)}>{index}</button
-    >
-  {/each}
-</div>
-
-<style>
-  h1 {
-    font-size: 2rem;
-    font-weight: 700;
-    transition: color 0.2s;
-  }
-
-  div {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    grid-gap: 5px;
-    max-width: 400px;
-  }
-
-  button {
-    aspect-ratio: 1;
-    border-radius: 50%;
-    background: var(--color, #fff);
-    transform: translate(-2px, -2px);
-    filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
-    transition: all 0.1s;
-    color: black;
-    font-weight: 700;
-    font-size: 2rem;
-  }
-
-  button[aria-current="true"] {
-    transform: none;
-    filter: none;
-    box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
-  }
-</style>
+{#each things as thing (thing.id)}
+  <Thing name={thing.name} />
+{/each}
--- src/Nested.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  const { counter = { count: "b mystery" } } = $props();
-</script>
-
-<h2>Nested Counter: {counter.count}</h2>
