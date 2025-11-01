f1927fc57a1d54a99f3b4e6c40bdb8a973339495
tensaidevx <shubham.gupta.npd@gmail.com>
2025-11-01 09:38:05 +0530

    feature/bind

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
-  import { roll } from "./utils.js";
-
-  let promise = $state(roll());
+  let name = $state("world");
+  let a = $state(1);
+  let b = $state(2);
 </script>
 
-<button onclick={() => (promise = roll())}> roll the dice </button>
-{#await promise}
-  <p>...rolling</p>
-{:then number}
-  <p>You rolled a {number}!</p>
-{:catch}
-  <p>There was an error rolling the dice.</p>
-{/await}
+<input bind:value={name} />
+<!-- use bind to attach handler with input element  -->
+
+<label>
+  <input type="number" bind:value={a} min="0" max="10" />
+  <input type="range" bind:value={a} min="0" max="10" />
+</label>
+
+<label>
+  <input type="number" bind:value={b} min="0" max="10" />
+  <input type="range" bind:value={b} min="0" max="10" />
+</label>
+
+<p>{a} + {b} = {a + b}</p>
+
+<h1>Hello {name}!</h1>
